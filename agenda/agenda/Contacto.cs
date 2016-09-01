ac8b10180616f5d6163fae5d563e90f00b5d5037
delgadoguadamuz <delgadoguadamuz@hotmail.com>
2016-09-01 16:58:05 -0600

    primer commit

---
+++ agenda/agenda/Contacto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace agenda
+{
+    class Contacto
+    {
+
+        string nombre;
+
+        string apellido;
+
+        string dirrecion;
+
+        int telefonoCasa;
+
+        int telefonoCelular;
+
+
+        public string Nombre { get; set; }
+
+        public string  Apellido { get; set; }
+
+        public string  Dirrecion { get; set; }
+
+        public int TelefonoCasa { get; set; }
+
+        public int TelefonoCelular { get; set; }
+
+
+    }
+}
